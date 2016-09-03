94b9b8723072964dcae7aae2581e1afc87bb489d
Zakaria Ridouh <zridouh@live.com>
2016-09-02 23:23:57 -0700

    add basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const BASE_URL = `http://www.animefreak.tv/`
+
+const constructUrl = (name, epiNum) => `${BASE_URL}/watch/${name}-episode-${epiNum}-english-dubbed-online-free`;
+
+exports.getUrl = constructUrl;
--- tests.js
+++ tests.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const anime = require('./index.js');
+
+let url = anime.getUrl('naruto', 2)
+
+if (url === 'http://www.animefreak.tv//watch/naruto-episode-2-english-dubbed-online-free') {
+  return 0;
+} else {
+  return 1;
+}
