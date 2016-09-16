e950a909069ca96f3561fc986b81dc306c8b3153
Zakaria Ridouh <zridouh@live.com>
2016-09-16 01:00:30 -0700

    clear up exceptions

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
 
 const BASE_URL = `http://www.animefreak.tv/`
 
-exports.getUrl = (baseUrl = BASE_URL, name, epiNum) => `${baseUrl}/watch/${name}-episode-${epiNum}-english-dubbed-online-free`;
+exports.getUrl = getUrl;
+exports.getNextEpisode = getNextEpisode;
+
+function getUrl (baseUrl = BASE_URL, name, epiNum) {
+  if (typeof name === 'string' && typeof epiNum === 'number') {
+    return `${baseUrl}/watch/${name}-episode-${epiNum}-english-dubbed-online-free`
+  }
+}
+
+function getUrl (baseUrl = BASE_URL, name, currentEpiNum) {
+  if (typeof name === 'string' && typeof epiNum === 'number') {
+    return `${baseUrl}/watch/${name}-episode-${currentEpiNum + 1}-english-dubbed-online-free`
+  }
+}
