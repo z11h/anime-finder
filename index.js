222a681b6ef87bdd89dd7968a40b24e7b0a2aaf3
Zakaria Ridouh <zridouh@live.com>
2016-10-14 03:58:45 -0700

    add got dep, use yarn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const got = require('got')
+
 const BASE_URL = `http://www.animefreak.tv/`
 
 function getUrl (baseUrl = BASE_URL, name, epiNum) {
@@ -12,6 +14,10 @@ function getNextEpisode (baseUrl = BASE_URL, name, currentEpiNum) {
   if (typeof name === 'string' && typeof epiNum === 'number') {
     return `${baseUrl}/watch/${name}-episode-${currentEpiNum + 1}-english-dubbed-online-free`
   }
+}
+
+function getVideoUrl (baseUrl = BASE_URL, name, currentEpiNum) {
+  // @todo: get the actual video URL
 }
 
 exports.getUrl = getUrl;
