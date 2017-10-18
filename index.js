5866bbfe1920cd449246f7435e0c5894c5fd56ec
Zakaria Ridouh <zridouh@live.com>
2017-10-18 02:12:17 -0500

    some slight changes -- wip

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const got = require('got')
 
 const BASE_URL = `http://www.animefreak.tv/` //reliable site
 
-function getUrl (baseUrl = BASE_URL, name, epiNum) {
+function getEpisodeUrl (baseUrl = BASE_URL, name, epiNum) {
   if (typeof name === 'string' && typeof epiNum === 'number') {
     return `${baseUrl}/watch/${name}-episode-${epiNum}-english-dubbed-online-free`
   } else {
+    return `${baseUrl}`
+  }
+}
+
+function getShowUrl (baseUrl = BASE_URL, name) {
+  if (typeof name === 'string') {
+    return `${baseUrl}/watch/${name}-episode-${epiNum}-english-dubbed-online-free`
+  } else {
+    return `${baseUrl}`
+  }
+}
+
+// @todo: full list of episodes accessable through if of "book-navigation-${release}" html element. use getElementById and parse
+function getLatestEpisode () { 
+   if (typeof name === 'string') {
+     // go to `${baseUrl}/watch/${name}-english-dubbed-online-free`
+     // parse "book-navigation-${release}" html element
+  } else {
     return `http://www.animefreak.tv/`
   }
+}
+
+function getReleaseId(name) {
+  // @todo
 }
 
 function getNextEpisode (baseUrl = BASE_URL, name, currentEpiNum) {
